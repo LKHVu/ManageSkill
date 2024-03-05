96c58854360d42492f9821aac687c898d8f7b629
maithanhtoan1289 <maithantoan1070@gmail.com>
2024-03-05 14:15:00 +0700

    commit create project

---
+++ src/main/java/com/example/manageskill/ManageSkillApplication.java
@@ -0,0 +1,13 @@
+package com.example.manageskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageSkillApplication.class, args);
+    }
+
+}
